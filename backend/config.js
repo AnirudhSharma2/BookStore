a753d1af433c53b884db1c429ffb4a562d6b4f27
Anirudh Sharma <121388246+AnirudhSharma2@users.noreply.github.com>
2024-10-01 16:42:37 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://2022pietcsanirudh017:root@books-store-mern.iride.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
